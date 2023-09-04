15d5274d3e89599bf0155aa9e6bfa1d2c9f6e4b2
Ally Juma Said <mahadsaid95@icloud.com>
2023-09-04 23:33:29 +0100

    adding a display for user and logout

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,11 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, SafeAreaView, Pressable } from 'react-native'
 import React from 'react'
+import { auth } from '../firebase'
+import { useNavigation } from '@react-navigation/native'
 
 const ProfileScreen = () => {
+    const user = auth.currentUser;
+    const navigation = useNavigation();
+    const signOutUser = () => {
+        auth.signOut().then(() => {
+            navigation.replace("Login")
+        }).catch((error) => alert(error.message))
+        console.log(err) 
+    }
+
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <SafeAreaView style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+     <Pressable style={{marginVertical:10}}>
+        <Text>Welcome{user.email}</Text>
+     </Pressable>
+     <Pressable onPress={signOutUser}>
+        <Text>Logout</Text>
+     </Pressable>
+    </SafeAreaView>
   )
 }
 
